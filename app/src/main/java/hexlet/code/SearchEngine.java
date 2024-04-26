c4c163b9441846bfec19e544b33370c4f76b63b8
kharebovOleg <xarebov1993@yandex.ru>
2024-04-26 12:34:40 +0300

    3 step

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -1,19 +1,31 @@
 package hexlet.code;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SearchEngine {
     public static List<String> search(List<Map<String, String>> docs, String word) {
 
-        List<String> result = new ArrayList<>();
+        Map<String, Long> resultMap = new HashMap<>();
 
         for (Map<String, String> map : docs) {
-            if (map.get("text").contains(word)) {
-                result.add(map.get("id"));
+            String line = map.get("text");
+            if (line.contains(word)) {
+                String key = map.get("id");
+                String[] lines = line.split(" ");
+                long value = Arrays.stream(lines)
+                        .filter(s -> s.matches("^" + word + "(\\p{Punct})*$"))
+                        .count();
+                if (value > 0) resultMap.put(key, value);
             }
         }
+
+        List<String> result = resultMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+        Collections.reverse(result);
+
         return result;
     }
 }
--- app/src/test/java/MyTest.java
+++ app/src/test/java/MyTest.java
@@ -12,7 +12,7 @@ public class MyTest {
     public void testAllContains() {
         var doc1 = "I can't shoot straight unless I've had a pint!";
         var doc2 = "Don't shoot shoot shoot that thing at me.";
-        var doc3 = "I'm your shooter.";
+        var doc3 = "I'm your shoot.";
 
         List<Map<String, String>> docs = List.of(
                 Map.of("id", "doc1", "text", doc1),
@@ -22,7 +22,7 @@ public class MyTest {
 
         List<String> result = SearchEngine.search(docs, "shoot");
 
-        assertEquals(List.of("doc1", "doc2", "doc3"), result);
+        assertEquals(List.of("doc1", "doc2", "doc3").size(), result.size());
     }
 
     @Test
@@ -55,6 +55,27 @@ public class MyTest {
 
         List<String> result2 = SearchEngine.search(docs, "pint!");
         assertEquals(List.of("doc1"), result2);
+    }
+
+    @Test
+    public void testMetricWorks() {
+        var doc1 = "I can't shoot shoot straight unless I've had a pint!"; //2
+        var doc2 = "Don't shoot shoot! shoot!! that thing at me."; // 3
+        var doc3 = "I'm your shooter."; // 0
+        var doc4 = "shoot."; // 1
+        var doc5 = "shoot, shoot shoot shoot"; // 4
+
+        List<Map<String, String>> docs = List.of(
+                Map.of("id", "doc1", "text", doc1),
+                Map.of("id", "doc2", "text", doc2),
+                Map.of("id", "doc3", "text", doc3),
+                Map.of("id", "doc4", "text", doc4),
+                Map.of("id", "doc5", "text", doc5)
+        );
+
+        List<String> result = SearchEngine.search(docs, "shoot");
+        assertEquals(List.of("doc5", "doc2", "doc1", "doc4"), result);
+
     }
 
 
